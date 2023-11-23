8b553ae9166d5426581e7fd9851b958386ff7cdf
Github ID <113992235+kangminsukdotcom@users.noreply.github.com>
2023-11-24 00:01:33 +0900

    date changed

--- eleventy.config.js
+++ eleventy.config.js
@@ -40,12 +40,12 @@ module.exports = function(eleventyConfig) {
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "2018-01-01");
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "YYYY-MM-DD");
 	});
 
 	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('2018-01-01');
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('YYYY-MM-DD');
 	});
 
 	// Get the first `n` elements of a collection.
