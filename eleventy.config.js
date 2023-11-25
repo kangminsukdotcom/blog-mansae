a391fc70a39cb83d413cfbfe92567cbefb73fabc
Github ID <113992235+kangminsukdotcom@users.noreply.github.com>
2023-11-26 08:39:09 +0900

    rss again

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,9 @@ const { DateTime } = require("luxon");
 const markdownItAnchor = require("markdown-it-anchor");
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(pluginRss);
+};
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
